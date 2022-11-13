2735ff0b27bd52623244ad4265aa97910b21f609
Suslov Danil <danya.sus@yandex.ru>
2022-11-13 22:46:25 +0300

    Add Owner gRPC server and client

---
+++ SOP.AgeClient/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Grpc.Net.Client;
+using SOP.AgeServer;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7052");
+var grpcClient = new Ager.AgerClient(channel);
+Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit).");
+while (true)
+{
+    Console.ReadKey(true);
+    var request = new AgeRequest
+    {
+        Email = "danya.sus@yandex.ru",
+        Name = "Danil",
+        Surname = "Suslov",
+        Birthday = "12-08-2000",
+        VehicleRegistration = "NEW123Z"
+    };
+
+    var reply = grpcClient.GetAge(request);
+    Console.WriteLine($"You are {reply.Years}, {reply.Months} months and {reply.Days} days ago");
+}
---
+++ SOP.AgeServer/Services/OwnerAgerService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace SOP.AgeServer.Services
+{
+    public class AgerService : Ager.AgerBase
+    {
+        private readonly ILogger<AgerService> _logger;
+
+        public AgerService(ILogger<AgerService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public override Task<AgeReply> GetAge(AgeRequest request, ServerCallContext context)
+        {
+            var birthday = DateTime.Parse(request.Birthday);
+            var years = DateTime.Now.Year - birthday.Year;
+            var months = DateTime.Now.Month - birthday.Month;
+            var days = DateTime.Now.Day - birthday.Day;
+
+            return Task.FromResult(new AgeReply() { Years = (uint)years, Months = (uint)months, Days = (uint)days });
+        }
+    }
+}
